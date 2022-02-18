f592f5d4cb296dfad6cf2fe13bea27e8c82b248d
Ridho Maulana <edoin21@gmail.com>
2022-02-18 23:23:24 +0700

    Initial Commit

---
+++ lib/src/app.dart
@@ -0,0 +1,26 @@
+import 'package:assignment_flutter/src/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(),
+      onGenerateRoute: (RouteSettings routeSettings) {
+        return MaterialPageRoute<void>(
+          settings: routeSettings,
+          builder: (BuildContext context) {
+            switch (routeSettings.name) {
+              default:
+                return const HomePage();
+            }
+          },
+        );
+      },
+    );
+  }
+}
---
+++ lib/src/data/source.dart
@@ -0,0 +1,23 @@
+const staticJson = [
+  {
+    "title": "✏ Personal To-do List",
+    "category": "Personal",
+    "date_updated": '2022-02-18T15:34:38.025Z',
+    "content":
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempor massa purus. Donec quis vestibulum arcu, vel finibus ex. Duis at porta purus, eu porta orci. Proin tempus massa non eleifend lobortis. Curabitur vehicula nunc nec risus consequat, a varius neque sagittis. Nam non risus at ex porttitor porttitor. Vestibulum quis finibus risus."
+  },
+  {
+    "title": "💪 Project To-do List",
+    "category": "Work",
+    "date_updated": '2022-02-17T15:34:38.025Z',
+    "content":
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempor massa purus. Donec quis vestibulum arcu, vel finibus ex. Duis at porta purus, eu porta orci. Proin tempus massa non eleifend lobortis. Curabitur vehicula nunc nec risus consequat, a varius neque sagittis. Nam non risus at ex porttitor porttitor. Vestibulum quis finibus risus."
+  },
+  {
+    "title": "🍽 Cooking Recipes",
+    "category": "Family",
+    "date_updated": '2022-02-16T15:34:38.025Z',
+    "content":
+        "Buat buttermilk dengan cara mencampur susu dengan air lemon, lalu diamkan selama 10 menit.\nCampur tepung terigu, gula, gula, vanili, baking powder, baking soda, dan garam. Aduk rata.\nTambahkan telur, buttermilk, dan margarin cair ke adonan kering. Aduk rata.\nPanaskan teflon anti lengket dengan api sedang.\n Tuang adonan pancake dengana sendok sayur ke teflon. Setelah berongga, balik adonan, lalu angkat dan sajikan."
+  },
+];
